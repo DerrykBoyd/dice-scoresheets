2fdbe156463c944b7cf967c94f1d223e555bd7cc
dboydgit <derrykboyd@gmail.com>
2018-11-20 03:16:37 -0700

    Updates

---
+++ precache-manifest.d208a3cad8d91f130a624836b087fa9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e1d9b3932378bad28fb",
+    "url": "/dice-scoresheets/static/js/runtime~main.3e1d9b39.js"
+  },
+  {
+    "revision": "60becf78e402b20985d7",
+    "url": "/dice-scoresheets/static/js/main.60becf78.chunk.js"
+  },
+  {
+    "revision": "789dccd4a04c28186d6e",
+    "url": "/dice-scoresheets/static/js/1.789dccd4.chunk.js"
+  },
+  {
+    "revision": "60becf78e402b20985d7",
+    "url": "/dice-scoresheets/static/css/main.4d22eac6.chunk.css"
+  },
+  {
+    "revision": "65aaee12a0cd4a0d044baa4aeab74a01",
+    "url": "/dice-scoresheets/index.html"
+  }
+];
