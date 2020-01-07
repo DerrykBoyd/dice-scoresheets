89e520c334e886dbc3b434459cf58ef38ac17069
Derryk Boyd <derrykboyd@gmail.com>
2020-01-07 01:55:58 -0700

    Updates

---
+++ precache-manifest.bb1d377a8eb4bbcd3afa59e88c02d260.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e45fedbd9f567f1926c0dcd220f1090",
+    "url": "/dice-scoresheets/index.html"
+  },
+  {
+    "revision": "9911aceb65f6d673dbeb",
+    "url": "/dice-scoresheets/static/css/main.8e97edef.chunk.css"
+  },
+  {
+    "revision": "6020cef3c9ab83c53989",
+    "url": "/dice-scoresheets/static/js/2.a6d369cb.chunk.js"
+  },
+  {
+    "revision": "fd626e487bbe73d17970b3deb5281d24",
+    "url": "/dice-scoresheets/static/js/2.a6d369cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9911aceb65f6d673dbeb",
+    "url": "/dice-scoresheets/static/js/main.f44ebd96.chunk.js"
+  },
+  {
+    "revision": "d3b5a26d429b9de26951",
+    "url": "/dice-scoresheets/static/js/runtime-main.48d3db74.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dice-scoresheets/precache-manifest.0004032b5d6c03138c9fa2c47e26ad0b.js"
+  "/dice-scoresheets/precache-manifest.bb1d377a8eb4bbcd3afa59e88c02d260.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dice-scoresheets/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dice-scoresheets/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
